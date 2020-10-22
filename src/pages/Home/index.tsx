90f4a3c872acd2226dec514d11cf7a419cee12e6
Guilherme <guilhermevictor20@gmail.com>
2020-10-22 00:31:40 -0300

    style(home components): separated parts into components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BiCurrentLocation } from 'react-icons/bi';
 import { MdLocationOn } from 'react-icons/md';
 
-import { Container } from './styles';
+import { Container, SideBar, CardList, Highlights } from './styles';
 
 import CloudBackground from '../../assets/images/Cloud-background.png';
 import ShowerImage from '../../assets/images/Shower.png';
@@ -10,7 +10,7 @@ import ShowerImage from '../../assets/images/Shower.png';
 const Home: React.FC = () => {
   return (
     <Container>
-      <aside>
+      <SideBar>
         <header>
           <button type="button">Search for places</button>
           <button type="button">
@@ -35,14 +35,14 @@ const Home: React.FC = () => {
             Hellsink
           </p>
         </footer>
-      </aside>
+      </SideBar>
       <div>
         <main>
           <header>
             <button type="button">ºC</button>
             <button type="button">ºF</button>
           </header>
-          <section>
+          <CardList>
             <div>
               <p>Tomorrow</p>
               <img src={ShowerImage} alt="Imagem do Clima" />
@@ -51,46 +51,14 @@ const Home: React.FC = () => {
                 <p>11ºC</p>
               </footer>
             </div>
-            <div>
-              <p>Tomorrow</p>
-              <img src={ShowerImage} alt="Imagem do Clima" />
-              <footer>
-                <p>16ºC</p>
-                <p>11ºC</p>
-              </footer>
-            </div>
-            <div>
-              <p>Tomorrow</p>
-              <img src={ShowerImage} alt="Imagem do Clima" />
-              <footer>
-                <p>16ºC</p>
-                <p>11ºC</p>
-              </footer>
-            </div>
-            <div>
-              <p>Tomorrow</p>
-              <img src={ShowerImage} alt="Imagem do Clima" />
-              <footer>
-                <p>16ºC</p>
-                <p>11ºC</p>
-              </footer>
-            </div>
-            <div>
-              <p>Tomorrow</p>
-              <img src={ShowerImage} alt="Imagem do Clima" />
-              <footer>
-                <p>16ºC</p>
-                <p>11ºC</p>
-              </footer>
-            </div>
-          </section>
+          </CardList>
           <h2>Today Highlights</h2>
-          <section className="highlights">
-            <div className="highlight">teste</div>
-            <div className="highlight">teste</div>
-            <div className="highlight">teste</div>
-            <div className="highlight">teste</div>
-          </section>
+          <Highlights>
+            <div>teste</div>
+            <div>teste</div>
+            <div>teste</div>
+            <div>teste</div>
+          </Highlights>
           <footer className="devchallenges-footer">
             Guilherme Victor @ DevChallenges.io
           </footer>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -5,106 +5,6 @@ export const Container = styled.div`
   width: 100vw;
   height: 100vh;
 
-  aside {
-    background: #1e213a;
-    max-width: 460px;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    overflow: hidden;
-
-    header {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      width: 100%;
-
-      button {
-        width: 161px;
-        height: 40px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        background: #6e707a;
-        color: #e7e7eb;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-        outline: transparent;
-        border: none;
-        font: 500 16px Raleway;
-        cursor: pointer;
-        margin: 42px 46px;
-
-        & + button {
-          border-radius: 50%;
-          width: 40px;
-          height: 40px;
-        }
-      }
-    }
-
-    main {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      justify-content: space-between;
-      width: 100%;
-      height: 100%;
-      position: relative;
-
-      div {
-        img + img {
-          position: absolute;
-          width: 650px;
-          height: 376px;
-          left: -160px;
-          top: 0;
-          opacity: 0.1;
-        }
-      }
-
-      p {
-        font-size: 144px;
-        font-style: normal;
-        font-weight: 500;
-        margin-bottom: 90px;
-
-        span {
-          font-size: 56px;
-          color: #a09fb1;
-        }
-      }
-
-      h1 {
-        font-weight: 600;
-        font-size: 36px;
-        line-height: 42px;
-        text-align: center;
-
-        color: #a09fb1;
-      }
-    }
-
-    footer {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      flex-direction: column;
-      width: 100%;
-      height: 200px;
-
-      p {
-        display: flex;
-        align-items: center;
-        font-weight: 500;
-        font-size: 18px;
-        color: #88869d;
-        line-height: 40px;
-      }
-    }
-  }
-
   div {
     flex: 1;
     display: flex;
@@ -142,61 +42,10 @@ export const Container = styled.div`
         }
       }
 
-      section {
-        display: flex;
-        justify-content: space-between;
-        width: 100%;
-
-        div {
-          height: 180px;
-          max-width: 120px;
-          display: flex;
-          align-items: center;
-          justify-content: space-between;
-          flex-direction: column;
-          background: #1e213a;
-          padding: 20px;
-
-          p {
-            font-weight: 500;
-            font-size: 16px;
-            color: #e7e7eb;
-          }
-
-          img {
-            width: 56px;
-            height: 62px;
-          }
-
-          footer {
-            display: flex;
-            align-items: center;
-            justify-content: space-between;
-            width: 100%;
-
-            p + p {
-              color: #a09fb1;
-            }
-          }
-        }
-      }
-
       h2 {
         align-self: flex-start;
         margin-top: 40px;
         font-size: 24px;
-      }
-
-      .highlights {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        gap: 30px;
-
-        .highlight {
-          max-width: 100%;
-          max-height: 204px;
-          background: #1e213a;
-        }
       }
 
       .devchallenges-footer {
@@ -208,5 +57,157 @@ export const Container = styled.div`
         color: #616475;
       }
     }
+  }
+`;
+
+export const SideBar = styled.aside`
+  background: #1e213a;
+  max-width: 460px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  overflow: hidden;
+
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+
+    button {
+      width: 161px;
+      height: 40px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: #6e707a;
+      color: #e7e7eb;
+      box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      outline: transparent;
+      border: none;
+      font: 500 16px Raleway;
+      cursor: pointer;
+      margin: 42px 46px;
+
+      & + button {
+        border-radius: 50%;
+        width: 40px;
+        height: 40px;
+      }
+    }
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+    height: 100%;
+    position: relative;
+
+    div {
+      img + img {
+        position: absolute;
+        width: 650px;
+        height: 376px;
+        left: -160px;
+        top: 0;
+        opacity: 0.1;
+      }
+    }
+
+    p {
+      font-size: 144px;
+      font-style: normal;
+      font-weight: 500;
+      margin-bottom: 90px;
+
+      span {
+        font-size: 56px;
+        color: #a09fb1;
+      }
+    }
+
+    h1 {
+      font-weight: 600;
+      font-size: 36px;
+      line-height: 42px;
+      text-align: center;
+
+      color: #a09fb1;
+    }
+  }
+
+  footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+    height: 200px;
+
+    p {
+      display: flex;
+      align-items: center;
+      font-weight: 500;
+      font-size: 18px;
+      color: #88869d;
+      line-height: 40px;
+    }
+  }
+`;
+
+export const CardList = styled.section`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  div {
+    height: 180px;
+    max-width: 120px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    flex-direction: column;
+    background: #1e213a;
+    padding: 20px;
+
+    p {
+      font-weight: 500;
+      font-size: 16px;
+      color: #e7e7eb;
+    }
+
+    img {
+      width: 56px;
+      height: 62px;
+    }
+
+    footer {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      width: 100%;
+
+      p + p {
+        color: #a09fb1;
+      }
+    }
+  }
+`;
+
+export const Highlights = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 30px;
+  width: 100%;
+
+  div {
+    width: 328px;
+    height: 204px;
+    background: #1e213a;
   }
 `;
